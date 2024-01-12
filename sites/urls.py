40c9d98708faf8387ff9c032c6580f3652bd05ad
mathias <mathiasguirao34@outlook.fr>
2024-01-12 11:49:38 +0100

    Add url of form

--- sites/urls.py
+++ sites/urls.py
@@ -6,5 +6,5 @@ from .views import ListeSitesView, AjouterSiteView
 urlpatterns = [
     path('sites/', ListeSitesView.as_view(), name='liste_sites'),
     path('ajouter_site/', AjouterSiteView.as_view(), name='ajouter_site'),
-
+    path('modifier_site/<int:pk>/', ModifierSiteView.as_view(), name='modifier_site'),
 ]
