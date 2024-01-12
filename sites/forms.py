96259bc57513fc5638cdcb43d59a386350ffe02d
mathias <mathiasguirao34@outlook.fr>
2024-01-12 11:21:07 +0100

    Add Form model

---
+++ sites/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Site
+
+
+class AjouterSiteForm(forms.ModelForm):
+    class Meta:
+        model = Site
+        fields = ['nom', 'url', 'identifiant', 'mot_de_passe']
