14d6f594193dbbd8e09202536aeddeaf3680efad
mathias <mathiasguirao34@outlook.fr>
2024-01-29 16:51:40 +0100

    add delete page to urls

--- sites/urls.py
+++ sites/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import ListeSitesView, ModifierSiteView
-from .views import ListeSitesView, AjouterSiteView
-
+from .views import ListeSitesView, AjouterSiteView, ModifierSiteView, SupprimerSiteView
 
 urlpatterns = [
     path('sites/', ListeSitesView.as_view(), name='liste_sites'),
     path('ajouter_site/', AjouterSiteView.as_view(), name='ajouter_site'),
     path('modifier_site/<int:pk>/', ModifierSiteView.as_view(), name='modifier_site'),
+    path('supprimer_site/<int:pk>/', SupprimerSiteView.as_view(), name='supprimer_site'),
 ]
